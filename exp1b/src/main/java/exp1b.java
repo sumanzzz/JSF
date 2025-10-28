cada9e24d959aa0191f6f3418479bb28e73fa5ca
Reegan Suman <145418786+sumanzzz@users.noreply.github.com>
2025-10-28 22:39:56 +0530

    Update exp1b.java

--- exp1b/src/main/java/exp1b.java
+++ exp1b/src/main/java/exp1b.java
@@ -95,12 +95,6 @@ public class exp1b extends HttpServlet {
 		
 	}
 	
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(false);
-		if(session != null) {
-			session.invalidate();
-		}
-		response.sendRedirect("exp1b");
-	}
+	
 
 }
